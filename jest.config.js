3f837370f1a15186302fbf8715dfb4e36032477b
Bioblaze Payne <bioblazepayne@gmail.com>
2026-02-11 18:14:28 -0700

    feat: configure Jest with node environment, coverage thresholds (#113)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    'src/server\\.js',
-  ],
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/node_modules/', 'src/server.js'],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
 };
